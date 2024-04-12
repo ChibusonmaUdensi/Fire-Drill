be684140a201dc170e65d4ae5e71f3b7c438a058
Mercy <sheroudensi@gmail.com>
2024-04-12 01:39:25 +0100

    snacks

---
+++ Kata.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+	public class Kata {
+ 	  public static main int compareTo(boolean arg); 
+if (number % 2 == 0) {
+	return true;
+}
+
+return false;
+
+}
+
+	  public static int args) {
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter a number:");
+	int number = input.nextInt();
+
+	}
+}

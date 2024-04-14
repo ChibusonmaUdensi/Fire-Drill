baa9ddc711c7075d5a9f15774fa94f67d2237c77
Mercy <sheroudensi@gmail.com>
2024-04-14 05:10:18 +0100

    snacks

--- Kata.java
+++ Kata.java
@@ -3,17 +3,16 @@ import java.lang.Math;
 
 
 public class Kata {
- 	  public static boolean isEven(int number) {
-          if (number % 2 == 0) {
-}
+ 	  public static boolean isEven(int number); 
+if (num % 2 == 0) {
 	return true;
 }
 
    public static int factorCount(int number) {
-	int factorCount = 0;
-      
-      for(int i = 1; i <= number; i++) {
-         if (number % i == 0) {
+      int result = 0;
+
+      for(int i = 1; i <= factorCount; i++) {
+         if (num % i == 0) {
             ++factorCount;
          }
       }
@@ -38,72 +37,66 @@ public class Kata {
    }
 
    public static boolean isPalindrome(int number) {
-      int sum=0,remainder;
-	while(number!=0) {
-	  remainder=number%10;
-	    sum=(sum*10)+remainder;
-	    number/=10;	
-}
-	return true;
+      int sum=0,r;
+	while(num!=0) {
+	  r=num%10;
+	    ssum=(sum*10)+r;
+	    num/=10;	
+	    }
+	return sum;
 	}
-   
-
-
-   public static long factorial(int number) {
-      long factorial = 1;
-
-      for(int i = 1; i <= number; ++i) {
-         factorial *= i;
-      }
-      return factorial;
-      }
-
-   public static int absoluteDiff(int number, int num2) {
-      int result = Math.abs(number - num2);
-      return result;
    }
 
-   public static double quotient(int number, int num2) {
-      double quotient = number / num2;
-      return quotient;
+   public static long factorial(int var0) {
+      long var1 = 1L;
+
+      for(int var3 = 1; var3 <= var0; ++var3) {
+         var1 *= (long)var3;
+      }
+
+      return var1;
    }
 
-   public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
+   public static int absoluteDiff(int var0, int var1) {
+      int var2 = Math.abs(var0 - var1);
+      return var2;
+   }
+
+   public static int quotient(int var0, int var1) {
+      int var2 = var0 / var1;
+      return var2;
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
       System.out.print("Enter an integer: ");
       int num = input.nextInt();
-
       if (isEven(num)) {
          System.out.println(num + " is even.");
       } else {
          System.out.println(num + " is odd.");
       }
 
-      System.out.printf("Factorial is %d%n", factorial(num));
-
+      System.out.printf("Factorial is %d", factorial(num));
       System.out.println("Number of factors " + factorCount(num));
- 
-     if (isPalindrome(num)) {
-         System.out.println(num + "is a Palindrome");
+      if (isPalindrome(var2)) {
+         System.out.print(num + "is a Palindrome");
       } else {
-         System.out.println(num + "is not a Palindrome");
+         System.out.print(num + "is not a Palindrome");
       }
 
       if (isPrime(num)) {
-         System.out.println(num + "is prime.");
+         System.out.println(var2 + "is prime.");
       } else {
-         System.out.println(num + "is not prime. ");
+         System.out.println(var2 + "is not prime. ");
       }
 
       System.out.print("Enter an integer:");
       int num2 = input.nextInt();
-    
-
-      System.out.print("Absolute diff: " + Math.abs(num -num2));
-
-
-      System.out.printf("quotient is : %.2f%n",  quotient(num,num2)); 
-  }
-
+      PrintStream var10000 = System.out;
+      int result = absoluteDiff(var2, var3);
+      System.out.print("Absolute diff: " + var10001);
+      System.out.printf("quotient is: " + quotient(number, num2));
+   }
 }
     

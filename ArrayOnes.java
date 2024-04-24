8c3c2ae54fafebf5ceb9ab697c09a3fd16255bd9
ChibusonmaUdensi <165843794+ChibusonmaUdensi@users.noreply.github.com>
2024-04-24 08:45:49 +0100

    ArrayOnes

---
+++ ArrayOnes.java
@@ -0,0 +1,26 @@
+public class ArrayOnes {
+	public static int[] Array (int[] numbers) {
+	int[] newArray ={4, 5, 8, 8, 2, 9};
+ 	for(int digit: newArray) {
+		if (digit % 2 = 0) {
+			digit = 0;
+		}
+		return newArray;
+	}  
+
+	for(int num: newArray) {
+		if (num % 2 = 1) {
+			num = 1;
+		}
+		return newArray;
+	}
+
+   }
+
+
+	public static void main(String[] args) {
+  		 int[] newArray ={4, 5, 8, 8, 2, 9};
+
+    System.out.print(newArray);
+	}
+  }
